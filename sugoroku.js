25e4e31bfcb9935bee04299db5d03ccdb5d7c29e
hoshi54hide <66288595+hoshi54hide@users.noreply.github.com>
2020-08-04 11:32:56 +0900

    Update sugoroku.js

--- sugoroku.js
+++ sugoroku.js
@@ -1,7 +1,8 @@
 
-img = new Array("★","◆","●","■");
+img = new Array("★", "◆", "●", "■");
 koma = new Array();
 goal = 23;
+leftValue = 0;
 
 restart();
 
@@ -10,9 +11,10 @@ function restart() {
 	pre_num = 0;
 	pre_r = 0;
 	timer = 0;
+	leftValue = 0;
 
-	for (i=0; i<4; i++) {
-		document.getElementById("brd"+i).innerHTML = img[i];
+	for (i = 0; i < 4; i++) {
+		document.getElementById("brd" + i).innerHTML = img[i];
 		koma[i] = 0;
 	}
 
@@ -21,16 +23,16 @@ function restart() {
 
 function getNum(num) {
 	clearTimeout(timer);
-	document.getElementById("sai"+pre_num).innerHTML = pre_r;
+	document.getElementById("sai" + pre_num).innerHTML = pre_r;
 	pre_num = num;
 	pre_r = r;
-	document.getElementById("sai"+num).innerHTML = "<font color='#FF0000'>" + r + "</font>";
+	document.getElementById("sai" + num).innerHTML = "<font color='#FF0000'>" + r + "</font>";
 	koma[num] += r;
 	var s = "";
-	for (i=0; i<koma[num]; i++) {
+	for (i = 0; i < koma[num]; i++) {
 		s += "🐾🐾";
 	}
-	document.getElementById("brd"+num).innerHTML = s + img[num];
+	document.getElementById("brd" + num).innerHTML = s + img[num];
 	if (koma[num] < goal) {
 		sai_next();
 	} else {
@@ -39,14 +41,17 @@ function getNum(num) {
 }
 
 function goalin() {
-	alert("ゴール！！\n優勝は" + document.form1.elements["user"+user_num].value + "さんです。");
-	restart();
+	alert("ゴール！！\n優勝は" + document.form1.elements["user" + user_num].value + "さんです。");
+	// リロードをさせて最初に戻す様にしています。
+	// もし不都合があれば変更をお願いします。
+	document.location.reload()
+	// restart();
 }
 
 function sai_next() {
 	user_num++;
 	if (user_num == 4) user_num = 0;
-	var s = document.form1.elements["user"+user_num].value;
+	var s = document.form1.elements["user" + user_num].value;
 	if (s != "") {
 		sai_reset();
 	} else {
@@ -63,12 +68,26 @@ function sai_reset() {
 	document.form1.btn1.disabled = true;
 	document.form1.btn2.disabled = true;
 	document.form1.btn3.disabled = true;
-	document.form1.elements["btn"+user_num].disabled = false;
+	document.form1.elements["btn" + user_num].disabled = false;
 	sai_start();
 }
 
 function sai_start() {
 	r = Math.floor(Math.random() * 6) + 1;
-	if (user_num != pre_num || pre_r == 0) document.getElementById("sai"+user_num).innerHTML = r;
-	timer = setTimeout("sai_start()",100);
+	if (user_num != pre_num || pre_r == 0) document.getElementById("sai" + user_num).innerHTML = r;
+	timer = setTimeout("sai_start()", 100);
+}
+
+// 残りのマスを計算する関数
+function getLeftVal() {
+	// サイコロの値を取得
+	var number = document.getElementById("sai0").textContent;
+	// 最初のマスはgoal値から引いていく
+	if (leftValue === 0) {
+		leftValue = goal - number;
+	} else {
+		leftValue = leftValue - number;
+	}
+	// 計算結果をid=leftvalに表示
+	document.getElementById("leftVal").innerHTML = leftValue;
 }
